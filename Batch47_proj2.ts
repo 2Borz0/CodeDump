cb21b5a9b802efdaac664d7cf0c3594f8793d346
Syed Hasan Gillani <139934362+2Borz0@users.noreply.github.com>
2023-07-22 12:16:42 +0500

    Add files via upload

---
+++ Batch47_proj2.ts
@@ -0,0 +1,12 @@
+// String in Uppercase 
+{
+let name = "asfandyar khan";
+ 
+let UppercaseName = name.toUpperCase();
+ 
+console.log(UppercaseName);
+
+// String Lowercase
+let LowercaseName = name.toLowerCase();
+console.log(LowercaseName);
+}
