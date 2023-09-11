9f9b03e720c7a6c7568eebfe3cf1a3a31c30efe0
Syed Hasan Gillani <139934362+2Borz0@users.noreply.github.com>
2023-09-11 08:18:21 +0500

    Add files via upload

---
+++ typescript proj 4 & 5.ts
@@ -0,0 +1,15 @@
+// Quote from a  Famous Person
+let Famous_Person = " Khalid bin Waleed R.A "
+let quote = `${Famous_Person} Said; May The Eyes of Cowards Never Sleep`
+console.log(quote)
+//Stripping Names
+
+let nameWithWhitespace= "\t John Doe \n";
+
+// Print the name with whitespace
+console.log("Name with whitespace", nameWithWhitespace );
+// Strip whitespace and print the name without it
+const strippedName= nameWithWhitespace.trim();
+
+console.log("Name after stripping whitespace:", strippedName);
+
