d5202d9fce67e3f03322ce0d9a93e685f4411cf0
Syed Hasan Gillani <139934362+2Borz0@users.noreply.github.com>
2023-09-19 14:49:32 +0500

    Add files via upload

---
+++ Batch47_Proj11_12.ts
@@ -0,0 +1,16 @@
+/* Ex#9; Favorite Number: Store your favorite number in a variable. Then, using that variable,
+ create a message that reveals your favorite number. Print that message.*/
+ { 
+    const FavNum = 7;
+ let SmS = `The Number ${FavNum} is my favorite number`
+ console.log(SmS);
+ }
+ /*Ex#11; Names: Store the names of a few of your friends in a array called names. 
+Print each person’s name by accessing each element in the list, one at a time.*/
+{ 
+    let friends: string[] = ['Nafay','Butt','Goofy','Lodhi']
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends[1]);
+console.log(friends[3]);
+}
